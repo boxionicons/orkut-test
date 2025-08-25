58beda152eb868d9e5e010998d16326c7a9d27e2
boxionicons <wahyustoreid112@gmail.com>
2025-08-25 07:14:48 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const chalk = require('chalk');
+const fs = require('fs');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.enable("trust proxy");
+app.set("json spaces", 2);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use('/', express.static(path.join(__dirname, 'view')));
+app.use('/api', express.static(path.join(__dirname, 'api')));
+
+const settingsPath = path.join(__dirname, './settings.json');
+const settings = JSON.parse(fs.readFileSync(settingsPath, 'utf-8'));
+
+global.apikey = settings.apikey || null;
+
+app.use((req, res, next) => {
+    const originalJson = res.json;
+    res.json = function (data) {
+        if (data && typeof data === 'object') {
+            const responseData = {
+                status: data.status,
+                creator: settings.apiSettings.creator || "Created Using Wahyu Store",
+                ...data
+            };
+            return originalJson.call(this, responseData);
+        }
+        return originalJson.call(this, data);
+    };
+    next();
+});
+
+// Api Route
+let totalRoutes = 0;
+const apiFolder = path.join(__dirname, './api');
+fs.readdirSync(apiFolder).forEach((subfolder) => {
+    const subfolderPath = path.join(apiFolder, subfolder);
+    if (fs.statSync(subfolderPath).isDirectory()) {
+        fs.readdirSync(subfolderPath).forEach((file) => {
+            const filePath = path.join(subfolderPath, file);
+            if (path.extname(file) === '.js') {
+                require(filePath)(app);
+                totalRoutes++;
+                console.log(chalk.bgHex('#FFFF99').hex('#333').bold(` Loaded Route: ${path.basename(file)} `));
+            }
+        });
+    }
+});
+console.log(chalk.bgHex('#90EE90').hex('#333').bold(' Load Complete! ✓ '));
+console.log(chalk.bgHex('#90EE90').hex('#333').bold(` Total Routes Loaded: ${totalRoutes} `));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(chalk.bgHex('#90EE90').hex('#333').bold(` Server is running on port ${PORT} `));
+});
+
+module.exports = app;
